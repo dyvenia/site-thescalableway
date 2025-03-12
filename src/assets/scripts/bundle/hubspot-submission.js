7120204af85cc0e05a09bcd21e1d19101321c7d5
madrilene <hola@lenesaile.com>
2025-03-12 11:28:53 +0100

    update newsletter form and author page, add to posts

--- src/assets/scripts/bundle/hubspot-submission.js
+++ src/assets/scripts/bundle/hubspot-submission.js
@@ -3,7 +3,16 @@ document.querySelector('[data-hubspot]').addEventListener('submit', async functi
 
   const form = event.target;
   const formData = new FormData(form);
-  const messageContainer = document.querySelector('[data-hubspot-message]');
+  const consentCheckbox = form.querySelector('input[name="consent"]');
+
+  // Message containers: hide all messages initially
+  document.querySelectorAll('[data-hubspot-message] > div').forEach(el => el.classList.add('hidden'));
+
+  // Ensure consent is checked before submitting
+  if (!consentCheckbox.checked) {
+    document.querySelector('[data-hubspot-consent-error]').classList.remove('hidden');
+    return;
+  }
 
   const data = {
     fields: Array.from(formData.entries()).map(([name, value]) => ({
@@ -12,23 +21,24 @@ document.querySelector('[data-hubspot]').addEventListener('submit', async functi
     }))
   };
 
-  const response = await fetch(
-    'https://api-eu1.hsforms.com/submissions/v3/integration/submit/143630396/b7e64c0d-fc4b-4940-bad4-adbf03471f8f',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-  );
+  try {
+    const response = await fetch(
+      'https://api-eu1.hsforms.com/submissions/v3/integration/submit/143630396/b7e64c0d-fc4b-4940-bad4-adbf03471f8f',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }
+    );
 
-  if (response.ok) {
-    messageContainer.textContent = '✅ Form submitted';
-    messageContainer.style.color = 'green';
-    form.reset();
-  } else {
-    messageContainer.textContent = '❌ Submission failed';
-    messageContainer.style.color = 'red';
+    document
+      .querySelector(response.ok ? '[data-hubspot-success]' : '[data-hubspot-error]')
+      .classList.remove('hidden');
+
+    if (response.ok) form.reset();
+  } catch (error) {
+    document.querySelector('[data-hubspot-error]').classList.remove('hidden');
   }
 });
