6e9a58bc6786386860f42f24d049611bb0605dc0
madrilene <hola@lenesaile.com>
2025-07-07 09:54:21 +0200

    wip: plausible prod

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,17 +1,5 @@
-const rawUrl = process.env.URL || process.env.DEPLOY_PRIME_URL || 'http://localhost:8080';
-const hostname = new URL(rawUrl).hostname;
-export const url = rawUrl;
-export const domain = hostname;
-export const isProduction = hostname === 'thescalableway.com';
-
-console.log('DEBUG:', {
-  URL: process.env.URL,
-  DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL,
-  NODE_ENV: process.env.NODE_ENV,
-  url,
-  domain,
-  isProduction
-});
+export const url = process.env.URL || 'http://localhost:8080';
+export const isProduction = process.env.CONTEXT === 'production';
 
 export const siteName = 'The Scalable Way';
 export const siteDescription = 'Empowering Data Platforms with Precision and Scale';
